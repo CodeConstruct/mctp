399f5125ba04f402d9f018682d87c523750a234f
Jeremy Kerr <jk@codeconstruct.com.au>
2021-05-05 10:01:38 +0800

    mctp-req: change to an echo client
    Expect a response from the peer, and compare the sent/received values.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- mctp-req.c
+++ mctp-req.c
@@ -11,8 +11,9 @@
 int main(void)
 {
 	struct _sockaddr_mctp addr;
+	socklen_t addrlen;
 	int rc, sd;
-	char c;
+	char c, r;
 
 	sd = socket(AF_MCTP, SOCK_DGRAM, 0);
 	if (sd < 0)
@@ -26,10 +27,31 @@ int main(void)
 
 	c = 0xaa;
 
+	/* send data */
 	rc = sendto(sd, &c, sizeof(c), 0,
 			(struct sockaddr *)&addr, sizeof(addr));
 	if (rc != sizeof(c))
 		err(EXIT_FAILURE, "sendto");
+
+	/* receive response */
+	addrlen = sizeof(addr);
+	rc = recvfrom(sd, &r, sizeof(r), 0,
+			(struct sockaddr *)&addr, &addrlen);
+	if (rc != sizeof(r))
+		err(EXIT_FAILURE, "recvfrom");
+
+	if (addrlen != sizeof(addr))
+		errx(EXIT_FAILURE, "unknown recv address length %d, exp %zd)",
+				addrlen, sizeof(addr));
+
+	printf("message from (net %d, eid %d): 0x%02x\n",
+			addr.smctp_network, addr.smctp_addr.s_addr,
+			r);
+
+	if (c != r)
+		errx(EXIT_FAILURE,
+				"payload mismatch; sent 0x%x, received 0x%x",
+				c, r);
 
 	return EXIT_SUCCESS;
 }
