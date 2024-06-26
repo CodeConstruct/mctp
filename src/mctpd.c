CodeConstruct/mctp

commit ea5c9a32a875cdbf8a8cb4cf8857689aa0a6146b

mctpd: Exit on control socket EOF
If we hit an EOF on the control socket, we'll keep getting
cb_listen_control_message() invoked in a loop, as there will always be a
POLLIN|POLLERR event pending.

Instead, just exit here.

Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -720,6 +720,9 @@ static int cb_listen_control_msg(sd_event_source *s, int sd, uint32_t revents,
 	if (rc < 0)
 		goto out;
 
+	if (rc == 0)
+		errx(EXIT_FAILURE, "Control socket returned EOF");
+
 	if (addr.smctp_base.smctp_type != MCTP_CTRL_HDR_MSG_TYPE) {
 		warnx("BUG: Wrong message type for listen socket");
 		rc = -EINVAL;

src/mctp.c
@@ -1309,6 +1309,7 @@ int main(int argc, char **argv)
 	if (!cmd)
 		errx(EXIT_FAILURE, "no such command '%s'", cmdname);
 
+	mctp_ops_init();
 	ctx->nl = mctp_nl_new(ctx->verbose);
 	if (!ctx->nl)
 		errx(EXIT_FAILURE, "Error creating netlink object");

commit 6a924addc9bd6ce330812323bbc411234ce821a8

mctpd: fix return value check in control socket reads
Currently, we're checking the return value of read_message against 0 to
detect EOF, but this isn't a length value. This means that any
successful read will hit the errx.

Instead, check the actual length, and improve the read_message case
where we see an EOF from the recvmsg().

Fixes: ea5c9a3 ("mctpd: Exit on control socket EOF")
Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -426,6 +426,13 @@ static int read_message(ctx *ctx, int sd, uint8_t **ret_buf, size_t *ret_buf_siz
 		goto out;
 	}
 
+	if (len == 0) {
+		*ret_buf = NULL;
+		*ret_buf_size = 0;
+		rc = 0;
+		goto out;
+	}
+
 	buf_size = len;
 	buf = malloc(buf_size);
 	if (!buf) {
@@ -721,7 +728,7 @@ static int cb_listen_control_msg(sd_event_source *s, int sd, uint32_t revents,
 	if (rc < 0)
 		goto out;
 
-	if (rc == 0)
+	if (buf_size == 0)
 		errx(EXIT_FAILURE, "Control socket returned EOF");
 
 	if (addr.smctp_base.smctp_type != MCTP_CTRL_HDR_MSG_TYPE) {