CodeConstruct/mctp

commit 3b1e91d04196444de4faa97c8b3b301a0e35e4e2

mctpd: Move BusOwner1 interface to MCTP interface objects
In 9b4eabc ("mctpd: use .DRAFT suffix on BusOwner1 interface"), we
renamed BusOwner1 to BusOwner1.DRAFT while it was present on the
top-level dbus object.

Now that we have the interface objects present, we can move the BusOwner
interfaces to their proper location on those interfaces, and drop the
DRAFT1 suffix.

Because we already know the object, we no longer need the interface name
string as the first argument to all calls.

Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -43,7 +43,7 @@
 #define MCTP_DBUS_PATH "/au/com/codeconstruct/mctp1"
 #define MCTP_DBUS_PATH_NETWORKS "/au/com/codeconstruct/mctp1/networks"
 #define MCTP_DBUS_PATH_LINKS "/au/com/codeconstruct/mctp1/interfaces"
-#define CC_MCTP_DBUS_IFACE_BUSOWNER "au.com.codeconstruct.MCTP.BusOwner1.DRAFT"
+#define CC_MCTP_DBUS_IFACE_BUSOWNER "au.com.codeconstruct.MCTP.BusOwner1"
 #define CC_MCTP_DBUS_IFACE_ENDPOINT "au.com.codeconstruct.MCTP.Endpoint1"
 #define CC_MCTP_DBUS_IFACE_TESTING "au.com.codeconstruct.MCTPTesting"
 #define MCTP_DBUS_NAME "au.com.codeconstruct.MCTP1"
@@ -1790,6 +1790,31 @@ out:
 	return rc;
 }
 
+static int interface_call_to_ifindex_busowner(ctx *ctx, sd_bus_message *msg)
+{
+	const char *iface, *path;
+	link_userdata *link;
+	int rc, ifindex;
+
+	path = sd_bus_message_get_path(msg);
+	if (!path)
+		return -1;
+
+	rc = sd_bus_path_decode_many(path, MCTP_DBUS_PATH_LINKS "/%", &iface);
+	if (rc <= 0)
+		return -1;
+
+	ifindex = mctp_nl_ifindex_byname(ctx->nl, iface);
+	if (ifindex < 0)
+		return -1;
+
+	link = mctp_nl_get_link_userdata(ctx->nl, ifindex);
+	if (!link || link->role != ENDPOINT_ROLE_BUS_OWNER)
+		return -1;
+
+	return ifindex;
+}
+
 static int validate_dest_phys(ctx *ctx, const dest_phys *dest)
 {
 	if (dest->hwaddr_len > MAX_ADDR_LEN) {
@@ -1831,24 +1856,19 @@ static int message_read_hwaddr(sd_bus_message *call, dest_phys* dest)
 static int method_setup_endpoint(sd_bus_message *call, void *data, sd_bus_error *berr)
 {
 	int rc;
-	const char *ifname = NULL;
 	dest_phys desti = {0}, *dest = &desti;
 	char *peer_path = NULL;
 	ctx *ctx = data;
 	peer *peer = NULL;
 
-	rc = sd_bus_message_read(call, "s", &ifname);
-	if (rc < 0)
-		goto err;
+	dest->ifindex = interface_call_to_ifindex_busowner(ctx, call);
+	if (dest->ifindex <= 0)
+		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
+			"Unknown MCTP interface");
 
 	rc = message_read_hwaddr(call, dest);
 	if (rc < 0)
 		goto err;
-
-	dest->ifindex = mctp_nl_ifindex_byname(ctx->nl, ifname);
-	if (dest->ifindex <= 0)
-		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
-			"Unknown MCTP ifname '%s'", ifname);
 
 	rc = validate_dest_phys(ctx, dest);
 	if (rc < 0)
@@ -1895,24 +1915,19 @@ err:
 static int method_assign_endpoint(sd_bus_message *call, void *data, sd_bus_error *berr)
 {
 	int rc;
-	const char *ifname = NULL;
 	dest_phys desti, *dest = &desti;
 	char *peer_path = NULL;
 	ctx *ctx = data;
 	peer *peer = NULL;
 
-	rc = sd_bus_message_read(call, "s", &ifname);
-	if (rc < 0)
-		goto err;
+	dest->ifindex = interface_call_to_ifindex_busowner(ctx, call);
+	if (dest->ifindex <= 0)
+		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
+			"Unknown MCTP interface");
 
 	rc = message_read_hwaddr(call, dest);
 	if (rc < 0)
 		goto err;
-
-	dest->ifindex = mctp_nl_ifindex_byname(ctx->nl, ifname);
-	if (dest->ifindex <= 0)
-		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
-			"Unknown MCTP ifname '%s'", ifname);
 
 	rc = validate_dest_phys(ctx, dest);
 	if (rc < 0)
@@ -1951,16 +1966,16 @@ static int method_assign_endpoint_static(sd_bus_message *call, void *data,
 					 sd_bus_error *berr)
 {
 	dest_phys desti, *dest = &desti;
-	const char *ifname = NULL;
 	char *peer_path = NULL;
 	peer *peer = NULL;
 	ctx *ctx = data;
 	uint8_t eid;
 	int rc;
 
-	rc = sd_bus_message_read(call, "s", &ifname);
-	if (rc < 0)
-		goto err;
+	dest->ifindex = interface_call_to_ifindex_busowner(ctx, call);
+	if (dest->ifindex <= 0)
+		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
+			"Unknown MCTP interface");
 
 	rc = message_read_hwaddr(call, dest);
 	if (rc < 0)
@@ -1969,11 +1984,6 @@ static int method_assign_endpoint_static(sd_bus_message *call, void *data,
 	rc = sd_bus_message_read(call, "y", &eid);
 	if (rc < 0)
 		goto err;
-
-	dest->ifindex = mctp_nl_ifindex_byname(ctx->nl, ifname);
-	if (dest->ifindex <= 0)
-		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
-			"Unknown MCTP ifname '%s'", ifname);
 
 	rc = validate_dest_phys(ctx, dest);
 	if (rc < 0)
@@ -2028,25 +2038,20 @@ err:
 static int method_learn_endpoint(sd_bus_message *call, void *data, sd_bus_error *berr)
 {
 	int rc;
-	const char *ifname = NULL;
 	char *peer_path = NULL;
 	dest_phys desti, *dest = &desti;
 	ctx *ctx = data;
 	peer *peer = NULL;
 	mctp_eid_t eid = 0;
 
-	rc = sd_bus_message_read(call, "s", &ifname);
-	if (rc < 0)
-		goto err;
+	dest->ifindex = interface_call_to_ifindex_busowner(ctx, call);
+	if (dest->ifindex <= 0)
+		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
+			"Unknown MCTP interface");
 
 	rc = message_read_hwaddr(call, dest);
 	if (rc < 0)
 		goto err;
-
-	dest->ifindex = mctp_nl_ifindex_byname(ctx->nl, ifname);
-	if (dest->ifindex <= 0)
-		return sd_bus_error_setf(berr, SD_BUS_ERROR_INVALID_ARGS,
-			"Unknown MCTP ifname '%s'", ifname);
 
 	rc = validate_dest_phys(ctx, dest);
 	if (rc < 0)
@@ -2673,12 +2678,11 @@ static int method_test_timer(sd_bus_message *call, void *data, sd_bus_error *sde
 	return rc;
 }
 
-static const sd_bus_vtable bus_mctpd_vtable[] = {
+static const sd_bus_vtable bus_owner_vtable[] = {
 	SD_BUS_VTABLE_START(0),
 
 	SD_BUS_METHOD_WITH_NAMES("SetupEndpoint",
-		"say",
-		SD_BUS_PARAM(ifname)
+		"ay",
 		SD_BUS_PARAM(physaddr),
 		"yisb",
 		SD_BUS_PARAM(eid)
@@ -2689,8 +2693,7 @@ static const sd_bus_vtable bus_mctpd_vtable[] = {
 		0),
 
 	SD_BUS_METHOD_WITH_NAMES("AssignEndpoint",
-		"say",
-		SD_BUS_PARAM(ifname)
+		"ay",
 		SD_BUS_PARAM(physaddr),
 		"yisb",
 		SD_BUS_PARAM(eid)
@@ -2701,8 +2704,7 @@ static const sd_bus_vtable bus_mctpd_vtable[] = {
 		0),
 
 	SD_BUS_METHOD_WITH_NAMES("AssignEndpointStatic",
-		"sayy",
-		SD_BUS_PARAM(ifname)
+		"ayy",
 		SD_BUS_PARAM(physaddr)
 		SD_BUS_PARAM(eid),
 		"yisb",
@@ -2714,8 +2716,7 @@ static const sd_bus_vtable bus_mctpd_vtable[] = {
 		0),
 
 	SD_BUS_METHOD_WITH_NAMES("LearnEndpoint",
-		"say",
-		SD_BUS_PARAM(ifname)
+		"ay",
 		SD_BUS_PARAM(physaddr),
 		"yisb",
 		SD_BUS_PARAM(eid)
@@ -3137,9 +3138,9 @@ static char* root_endpoints_path(int net)
 }
 
 /* au.com.CodeConstruct.MCTP.Interface1 interface */
-static int bus_mctp_link_find(sd_bus *bus, const char *path,
-	const char *interface, void *userdata, void **ret_found,
-	sd_bus_error *ret_error)
+static int __bus_mctp_link_find(sd_bus *bus, const char *path,
+	const char *interface, void *userdata, bool owner_only,
+	void **ret_found, sd_bus_error *ret_error)
 {
 	ctx *ctx = userdata;
 	char *tmpstr = NULL;
@@ -3164,12 +3165,34 @@ static int bus_mctp_link_find(sd_bus *bus, const char *path,
 		return -ENOMEM;
 	}
 
+	printf("%s: owner_only %d, role %d\n", __func__, owner_only,
+	       lmUserData->role);
+
+	if (owner_only && lmUserData->role != ENDPOINT_ROLE_BUS_OWNER)
+		return 0;
+
 	if (lmUserData->published) {
 		*ret_found = ctx;
 		return 1;
 	}
 
 	return 0;
+}
+
+static int bus_mctp_link_find(sd_bus *bus, const char *path,
+	const char *interface, void *userdata,
+	void **ret_found, sd_bus_error *ret_error)
+{
+	return __bus_mctp_link_find(bus, path, interface, userdata, false,
+				    ret_found, ret_error);
+}
+
+static int bus_mctp_link_busowner_find(sd_bus *bus, const char *path,
+	const char *interface, void *userdata, void **ret_found,
+	sd_bus_error *ret_error)
+{
+	return __bus_mctp_link_find(bus, path, interface, userdata, true,
+				    ret_found, ret_error);
 }
 
 static char* net_path(int net)
@@ -3517,16 +3540,6 @@ static int setup_bus(ctx *ctx)
 
 	/* mctp object needs to use _fallback_vtable() since we can't
 	   mix non-fallback and fallback vtables on MCTP_DBUS_PATH */
-	rc = sd_bus_add_fallback_vtable(ctx->bus, NULL,
-					MCTP_DBUS_PATH,
-					CC_MCTP_DBUS_IFACE_BUSOWNER,
-					bus_mctpd_vtable,
-					bus_mctpd_find,
-					ctx);
-	if (rc < 0) {
-		warnx("Failed creating D-Bus object");
-		goto out;
-	}
 
 	rc = sd_bus_add_fallback_vtable(ctx->bus, NULL,
 					MCTP_DBUS_PATH,
@@ -3573,6 +3586,16 @@ static int setup_bus(ctx *ctx)
 		goto out;
 	}
 
+	rc = sd_bus_add_fallback_vtable(ctx->bus, NULL,
+					MCTP_DBUS_PATH,
+					CC_MCTP_DBUS_IFACE_BUSOWNER,
+					bus_owner_vtable,
+					bus_mctp_link_busowner_find,
+					ctx);
+	if (rc < 0) {
+		warnx("Failed creating D-Bus object");
+		goto out;
+	}
 	rc = sd_bus_add_object_manager(ctx->bus, NULL, MCTP_DBUS_PATH);
 	if (rc < 0) {
 		warnx("Adding object manager failed: %s", strerror(-rc));

tests/mctp_test_utils.py
@@ -1,10 +1,10 @@
 
-async def mctpd_mctp_obj(dbus):
+async def mctpd_mctp_iface_obj(dbus, iface):
     obj = await dbus.get_proxy_object(
             'au.com.codeconstruct.MCTP1',
-            '/au/com/codeconstruct/mctp1'
+            '/au/com/codeconstruct/mctp1/interfaces/' + iface.name
         )
-    return await obj.get_interface('au.com.codeconstruct.MCTP.BusOwner1.DRAFT')
+    return await obj.get_interface('au.com.codeconstruct.MCTP.BusOwner1')
 
 async def mctpd_mctp_endpoint_obj(dbus, path):
     obj = await dbus.get_proxy_object(

tests/test_mctpd.py
@@ -3,7 +3,7 @@ import trio
 import uuid
 import asyncdbus
 
-from mctp_test_utils import mctpd_mctp_obj, mctpd_mctp_endpoint_obj
+from mctp_test_utils import mctpd_mctp_iface_obj, mctpd_mctp_endpoint_obj
 from conftest import Endpoint
 
 # DBus constant symbol suffixes:
@@ -13,7 +13,7 @@ from conftest import Endpoint
 # - I: Interface
 MCTPD_C = 'au.com.codeconstruct.MCTP1'
 MCTPD_MCTP_P = '/au/com/codeconstruct/mctp1'
-MCTPD_MCTP_I = 'au.com.codeconstruct.MCTP.BusOwner1.DRAFT'
+MCTPD_MCTP_I = 'au.com.codeconstruct.MCTP.BusOwner1'
 MCTPD_ENDPOINT_I = 'au.com.codeconstruct.MCTP.Endpoint1'
 DBUS_OBJECT_MANAGER_I = 'org.freedesktop.DBus.ObjectManager'
 DBUS_PROPERTIES_I = 'org.freedesktop.DBus.Properties'
@@ -28,8 +28,8 @@ the new kernel neighbour and route entries.
 
 We have a few things provided by the test infrastructure:
 
- - dbus is the dbus connection, call the mctpd_mctp_obj helper to
-   get the MCTP dbus object
+ - dbus is the dbus connection, call the mctpd_mctp_iface_obj helper to
+   get the MCTP dbus interface object
 
  - mctpd is our wrapper for the mctpd process and mock MCTP environment. This
    has two properties that represent external state:
@@ -64,10 +64,10 @@ async def test_setup_endpoint(dbus, mctpd):
     ep = mctpd.network.endpoints[0]
 
     # our proxy dbus object for mctpd
-    mctp = await mctpd_mctp_obj(dbus)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
 
     # call SetupEndpoint. This will raise an exception on any dbus error.
-    (eid, net, path, new) = await mctp.call_setup_endpoint(iface.name, ep.lladdr)
+    (eid, net, path, new) = await mctp.call_setup_endpoint(ep.lladdr)
 
     # ep.eid will be updated (through the Set Endpoint ID message); this
     # should match the returned EID
@@ -93,26 +93,27 @@ We have the following scenario:
 At (3), we should reconfigure the EID to B.
 """
 async def test_setup_endpoint_conflict(dbus, mctpd):
-    mctp = await mctpd_mctp_obj(dbus)
-
     iface = mctpd.system.interfaces[0]
+
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
+
     ep1 = mctpd.network.endpoints[0]
-    (eid1, _, _, _) = await mctp.call_setup_endpoint(iface.name, ep1.lladdr)
+    (eid1, _, _, _) = await mctp.call_setup_endpoint(ep1.lladdr)
 
     # endpoint configured with eid1 already
     ep2 = Endpoint(iface, bytes([0x1e]), eid=eid1)
     mctpd.network.add_endpoint(ep2)
 
-    (eid2, _, _, _) = await mctp.call_setup_endpoint(iface.name, ep2.lladdr)
+    (eid2, _, _, _) = await mctp.call_setup_endpoint(ep2.lladdr)
     assert eid1 != eid2
 
 """ Test neighbour removal """
 async def test_remove_endpoint(dbus, mctpd):
-    mctp = await mctpd_mctp_obj(dbus)
-
     iface = mctpd.system.interfaces[0]
     ep1 = mctpd.network.endpoints[0]
-    (_, _, path, _) = await mctp.call_setup_endpoint(iface.name, ep1.lladdr)
+
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
+    (_, _, path, _) = await mctp.call_setup_endpoint(ep1.lladdr)
 
     assert(len(mctpd.system.neighbours) == 1)
 
@@ -124,8 +125,8 @@ async def test_remove_endpoint(dbus, mctpd):
 async def test_recover_endpoint_present(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
-    mctp = await mctpd_mctp_obj(dbus)
-    (eid, net, path, new) = await mctp.call_setup_endpoint(iface.name, dev.lladdr)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
+    (eid, net, path, new) = await mctp.call_setup_endpoint(dev.lladdr)
 
     ep = await dbus.get_proxy_object(MCTPD_C, path)
     ep_props = await ep.get_interface(DBUS_PROPERTIES_I)
@@ -152,8 +153,8 @@ async def test_recover_endpoint_removed(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
     mctp = await dbus.get_proxy_object(MCTPD_C, MCTPD_MCTP_P)
-    mctp_mctp = await mctp.get_interface(MCTPD_MCTP_I)
-    (eid, net, path, new) = await mctp_mctp.call_setup_endpoint(iface.name, dev.lladdr)
+    mctp_iface = await mctpd_mctp_iface_obj(dbus, iface)
+    (eid, net, path, new) = await mctp_iface.call_setup_endpoint(dev.lladdr)
 
     ep = await dbus.get_proxy_object(MCTPD_C, path)
     ep_props = await ep.get_interface(DBUS_PROPERTIES_I)
@@ -189,8 +190,8 @@ async def test_recover_endpoint_reset(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
     mctp = await dbus.get_proxy_object(MCTPD_C, MCTPD_MCTP_P)
-    mctp_mctp = await mctp.get_interface(MCTPD_MCTP_I)
-    (eid, net, path, new) = await mctp_mctp.call_setup_endpoint(iface.name, dev.lladdr)
+    mctp_iface = await mctpd_mctp_iface_obj(dbus, iface)
+    (eid, net, path, new) = await mctp_iface.call_setup_endpoint(dev.lladdr)
 
     ep = await dbus.get_proxy_object(MCTPD_C, path)
     ep_props = await ep.get_interface(DBUS_PROPERTIES_I)
@@ -225,8 +226,8 @@ async def test_recover_endpoint_exchange(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
     mctp = await dbus.get_proxy_object(MCTPD_C, MCTPD_MCTP_P)
-    mctp_mctp = await mctp.get_interface(MCTPD_MCTP_I)
-    (eid, net, path, new) = await mctp_mctp.call_setup_endpoint(iface.name, dev.lladdr)
+    mctp_iface = await mctpd_mctp_iface_obj(dbus, iface)
+    (eid, net, path, new) = await mctp_iface.call_setup_endpoint(dev.lladdr)
 
     ep = await dbus.get_proxy_object(MCTPD_C, path)
     ep_props = await ep.get_interface(DBUS_PROPERTIES_I)
@@ -279,11 +280,10 @@ on an AssignEndpointStatic call """
 async def test_assign_endpoint_static(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
-    mctp = await mctpd_mctp_obj(dbus)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
     static_eid = 12
 
     (eid, _, _, new) = await mctp.call_assign_endpoint_static(
-        iface.name,
         dev.lladdr,
         static_eid
     )
@@ -301,12 +301,11 @@ async def test_assign_endpoint_static(dbus, mctpd):
 EID"""
 async def test_assign_endpoint_static_allocated(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
-    mctp = await mctpd_mctp_obj(dbus)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
     dev = mctpd.network.endpoints[0]
     static_eid = 12
 
     (eid, _, _, new) = await mctp.call_assign_endpoint_static(
-        iface.name,
         dev.lladdr,
         static_eid,
     )
@@ -316,7 +315,6 @@ async def test_assign_endpoint_static_allocated(dbus, mctpd):
 
     # repeat, same EID
     (eid, _, _, new) = await mctp.call_assign_endpoint_static(
-        iface.name,
         dev.lladdr,
         static_eid,
     )
@@ -327,7 +325,7 @@ async def test_assign_endpoint_static_allocated(dbus, mctpd):
 """ Test that we cannot assign a conflicting static EID """
 async def test_assign_endpoint_static_conflict(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
-    mctp = await mctpd_mctp_obj(dbus)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
     dev1 = mctpd.network.endpoints[0]
 
     dev2 = Endpoint(iface, bytes([0x1e]))
@@ -335,7 +333,6 @@ async def test_assign_endpoint_static_conflict(dbus, mctpd):
 
     # dynamic EID assigment for dev1
     (eid, _, _, new) = await mctp.call_assign_endpoint(
-        iface.name,
         dev1.lladdr,
     )
 
@@ -343,7 +340,7 @@ async def test_assign_endpoint_static_conflict(dbus, mctpd):
 
     # try to assign dev2 with the dev1's existing EID
     with pytest.raises(asyncdbus.errors.DBusError) as ex:
-        await mctp.call_assign_endpoint_static(iface.name, dev2.lladdr, eid)
+        await mctp.call_assign_endpoint_static(dev2.lladdr, eid)
 
     assert str(ex.value) == "Address in use"
 
@@ -352,11 +349,10 @@ a different EID allocated"""
 async def test_assign_endpoint_static_varies(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
-    mctp = await mctpd_mctp_obj(dbus)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
     static_eid = 12
 
     (eid, _, _, new) = await mctp.call_assign_endpoint_static(
-        iface.name,
         dev.lladdr,
         static_eid
     )
@@ -365,7 +361,7 @@ async def test_assign_endpoint_static_varies(dbus, mctpd):
     assert new
 
     with pytest.raises(asyncdbus.errors.DBusError) as ex:
-        await mctp.call_assign_endpoint_static(iface.name, dev.lladdr, 13)
+        await mctp.call_assign_endpoint_static(dev.lladdr, 13)
 
     assert str(ex.value) == "Already assigned a different EID"
 
@@ -374,7 +370,7 @@ for a basic Get Endpoint ID command"""
 async def test_get_endpoint_id(dbus, mctpd):
     iface = mctpd.system.interfaces[0]
     dev = mctpd.network.endpoints[0]
-    mctp = await mctpd_mctp_obj(dbus)
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
     dev.eid = 12
 
     mctpd.system.add_route(mctpd.system.Route(iface, dev.eid, 0))

commit 8b8b834030407f0a0ee9f7d0939e4f428f9c23fa

mctpd: remove unnecessary debug output
Commit 3b1e91d04 added a debug output message, which is not useful for
general log output; remove it.

Fixes: 3b1e91d ("mctpd: Move BusOwner1 interface to MCTP interface objects")
Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -3282,9 +3282,6 @@ static int __bus_mctp_link_find(sd_bus *bus, const char *path,
 		return -ENOMEM;
 	}
 
-	printf("%s: owner_only %d, role %d\n", __func__, owner_only,
-	       lmUserData->role);
-
 	if (owner_only && lmUserData->role != ENDPOINT_ROLE_BUS_OWNER)
 		return 0;
 