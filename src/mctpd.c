CodeConstruct/mctp

commit 7ec2f8daa3a8948066390aee621d6afa03f6ecd9

mctpd: Add support for endpoint recovery
DSP0236 v1.3.1[1] 8.17.6 defines a process for reclaiming dynamically
assigned EIDs from endpoint devices that have been removed from the
network. The fundamental process is captured in the following paragraphs:

> - The bus owner shall wait at least `Treclaim` seconds before
>   reassigning a given EID (where `Treclaim` is specified in the
>   physical transport binding specification for the medium used to
>   access the endpoint).
>
> - Reclaimed EIDs shall only be reassigned after all unused EIDs in the
>   EID pool have been assigned to endpoints. Optionally, additional
>   robustness can be achieved if the bus owner maintains a short FIFO
>   list of reclaimed EIDs (and their associated physical addresses) and
>   allocates the older EIDs first.
>
> - A bus owner shall confirm that an endpoint has been removed by
>   attempting to access it after `Treclaim` has expired. It can do this
>   by issuing a `Get Endpoint ID` command to the endpoint to verify
>   that the endpoint is still non-responsive. It is recommended that
>   this be done at least three times, with a delay of at least `1/2 *
>   Treclaim` between tries if possible. If the endpoint continues to be
>   non-responsive, it can be assumed that it is safe to return its EID
>   to the pool of EIDs available for assignment.

[1]: https://www.dmtf.org/sites/default/files/standards/documents/DSP0236_1.3.1.pdf

The extract is framed with the perspective of reclaiming an unused EID
but we must consider the broader case of unresponsiveness - we don't yet
understand whether an EID should even be reclaimed. However, we can use
the mechanisms outlined - polling using `Get Endpoint ID` - to achieve
this understanding.

We wish to avoid continuous polling of devices by `mctpd` for a few
reasons:

1. Interleaving of commands to simple endpoints that may not cope
2. The increase in bus utilisation increases the probability of
   contention and loss of arbitration

src/mctpd.c
@@ -6,6 +6,9 @@
  * Copyright (c) 2021 Google
  */
 
+#include <assert.h>
+#include <bits/time.h>
+#include <systemd/sd-bus-vtable.h>
 #define _GNU_SOURCE
 
 #include <sys/socket.h>
@@ -112,6 +115,17 @@ struct peer {
 
 	// Stuff the ctx pointer into peer for tidier parameter passing
 	struct ctx *ctx;
+
+	// Connectivity state
+	bool degraded;
+	struct {
+		uint64_t delay;
+		sd_event_source *source;
+		int npolls;
+		mctp_eid_t eid;
+		uint8_t endpoint_type;
+		uint8_t medium_spec;
+	} recovery;
 };
 typedef struct peer peer;
 
@@ -1240,6 +1254,17 @@ static int remove_peer(peer *peer)
 	unpublish_peer(peer);
 
 	// Clear it
+	if (peer->degraded) {
+		int rc;
+
+		rc = sd_event_source_set_enabled(peer->recovery.source, SD_EVENT_OFF);
+		if (rc < 0) {
+			/* XXX: Fix caller assumptions? */
+			warnx("Failed to stop recovery timer while removing peer: %d", rc);
+		}
+		sd_event_source_unref(peer->recovery.source);
+	}
+
 	n->peeridx[peer->eid] = -1;
 	free(peer->message_types);
 	free(peer->uuid);
@@ -1585,6 +1610,46 @@ static int peer_set_uuid(peer *peer, const uint8_t uuid[16])
 	}
 	memcpy(peer->uuid, uuid, 16);
 	return 0;
+}
+
+static int
+query_get_peer_uuid_by_phys(ctx *ctx, const dest_phys *dest, uint8_t uuid[16])
+{
+	struct sockaddr_mctp_ext addr;
+	struct mctp_ctrl_cmd_get_uuid req;
+	struct mctp_ctrl_resp_get_uuid *resp = NULL;
+	uint8_t* buf = NULL;
+	size_t buf_size;
+	int rc;
+
+	req.ctrl_hdr.rq_dgram_inst = RQDI_REQ;
+	req.ctrl_hdr.command_code = MCTP_CTRL_CMD_GET_ENDPOINT_UUID;
+
+	rc = endpoint_query_phys(ctx, dest, MCTP_CTRL_HDR_MSG_TYPE,
+		&req, sizeof(req), &buf, &buf_size, &addr);
+	if (rc < 0)
+		goto out;
+
+	if (buf_size != sizeof(*resp)) {
+		warnx("%s: wrong reply %zu bytes. dest %s", __func__, buf_size,
+			dest_phys_tostr(dest));
+		rc = -ENOMSG;
+		goto out;
+	}
+	resp = (void*)buf;
+
+	if (resp->completion_code != 0x00) {
+		warnx("Failure completion code 0x%02x from %s",
+			resp->completion_code, dest_phys_tostr(dest));
+		rc = -ECONNREFUSED;
+		goto out;
+	}
+
+	memcpy(uuid, resp->uuid, 16);
+
+out:
+	free(buf);
+	return rc;
 }
 
 static int query_get_peer_uuid(peer *peer) {
@@ -2157,6 +2222,205 @@ out:
 	return rc;
 }
 
+/* FIXME: I2C-specific */
+/* DSP0237 v1.2.0 Table 9 */
+#define MCTP_I2C_TSYM_TRECLAIM_MIN_US 5000000
+#define MCTP_I2C_TSYM_MN1_MIN 2
+#define MCTP_I2C_TSYM_MT1_MAX_US 100000
+#define MCTP_I2C_TSYM_MT3_MAX_US 100000
+#define MCTP_I2C_TSYM_MT4_MIN_US 5000000
+#define MCTP_I2C_TSYM_MT2_MIN_US \
+	(MCTP_I2C_TSYM_MT1_MAX_US + 2 * MCTP_I2C_TSYM_MT3_MAX_US)
+#define MCTP_I2C_TSYM_MT2_MAX_MS MCTP_I2C_TSYM_MT4_MIN_US
+
+static int
+peer_endpoint_recover(sd_event_source *s, uint64_t usec, void *userdata)
+{
+	int ev_state __attribute__((unused));
+	peer *peer = userdata;
+	ctx *ctx = peer->ctx;
+	char *peer_path;
+	int rc;
+
+	/*
+	 * Error handling policy:
+	 *
+	 * 1. Any resource management error prior to Treclaim is handled by
+	 *    rescheduling the poll query, unless it is scheduling the poll
+	 *    query itself that fails.
+	 *
+	 * 2. If scheduling the poll query fails then the endpoint is removed.
+	 */
+
+	peer->recovery.npolls--;
+
+	/*
+	 * Test if we still have connectivity to the endpoint. If we do, we will get a
+	 * response reporting the current EID. This is the test recommended by 8.17.6
+	 * of DSP0236 v1.3.1.
+	 */
+	rc = query_get_endpoint_id(ctx, &peer->phys, &peer->recovery.eid,
+		&peer->recovery.endpoint_type, &peer->recovery.medium_spec);
+	if (rc < 0) {
+		goto reschedule;
+	}
+
+	/*
+	 * If we've got a response there are two scenarios:
+	 *
+	 * 1. The device responds with the EID that we expect it to have
+	 * 2. The device responds with an unexpected EID, e.g. 0
+	 *
+	 * For scenario 1 we're done as the device is responsive and has the expected
+	 * address. For scenario 2, we may not yet consider the EID assignment as
+	 * expired, so check the UUID for a match. If the UUID matches we reassign the
+	 * expected EID to the device. If the UUID does not match we allocate a new
+	 * EID for the exchanged device, given it is responsive.
+	 */
+	if (peer->recovery.eid != peer->eid) {
+		static const uint8_t nil_uuid[16] = {0};
+		uint8_t uuid[16] = {0};
+		bool uuid_matches_peer;
+		bool uuid_matches_nil;
+		mctp_eid_t new_eid;
+
+		rc = query_get_peer_uuid_by_phys(ctx, &peer->phys, uuid);
+
+		static_assert(sizeof(uuid) == sizeof(nil_uuid), "Unsynchronized UUID sizes");
+
+		/*
+		 * The peer must be published for .Recover to be called, so peer->uuid must
+		 * be valid
+		 */
+		assert(peer->uuid != NULL);
+
+		/*
+		 * The memory is always valid so the memcmp() isn't unsafe, but they are only
+		 * meaningful if query_get_peer_uuid_by_phys() succeeds
+		 */
+		uuid_matches_peer = memcmp(uuid, peer->uuid, sizeof(uuid)) == 0;
+		uuid_matches_nil = memcmp(uuid, nil_uuid, sizeof(uuid)) == 0;
+
+		if (rc == 0 && !uuid_matches_nil && uuid_matches_peer) {
+			/* Confirmation of the same device, apply it's already allocated EID */
+			rc = endpoint_send_set_endpoint_id(peer, &new_eid);
+			if (rc < 0) {
+				goto reschedule;
+			}
+
+			if (new_eid != peer->eid) {
+				rc = change_peer_eid(peer, new_eid);
+				if (rc < 0) {
+					goto reclaim;
+				}
+			}
+		} else {
+			/* It's not known to be the same device, allocate a new EID */
+			dest_phys phys = peer->phys;
+
+			assert(sd_event_source_get_enabled(peer->recovery.source, &ev_state) == 0);
+			remove_peer(peer);
+			rc = endpoint_assign_eid(ctx, NULL, &phys, NULL);
+			if (rc < 0) {
+				goto reschedule;
+			}
+		}
+	}
+
+	peer->degraded = false;
+
+	rc = path_from_peer(peer, &peer_path);
+	if (rc < 0) {
+		goto reschedule;
+	}
+
+	rc = sd_bus_emit_properties_changed(ctx->bus, peer_path,
+		CC_MCTP_DBUS_IFACE_ENDPOINT, "Connectivity", NULL);
+	free(peer_path);
+	if (rc < 0) {
+		goto reschedule;
+	}
+
+	assert(sd_event_source_get_enabled(peer->recovery.source, &ev_state) == 0);
+	sd_event_source_unref(peer->recovery.source);
+	peer->recovery.delay = 0;
+	peer->recovery.source = NULL;
+	peer->recovery.npolls = 0;
+
+	return rc;
+
+reschedule:
+	if (peer->recovery.npolls > 0) {
+		rc = sd_event_source_set_time_relative(peer->recovery.source,
+			peer->recovery.delay);
+		if (rc >= 0) {
+			rc = sd_event_source_set_enabled(peer->recovery.source, SD_EVENT_ONESHOT);
+		}
+	}
+	if (rc < 0) {
+reclaim:
+		/* Recovery unsuccessful, clean up the peer */
+		assert(sd_event_source_get_enabled(peer->recovery.source, &ev_state) == 0);
+		remove_peer(peer);
+	}
+	return rc < 0 ? rc : 0;
+}
+
+static int method_endpoint_recover(sd_bus_message *call, void *data,
+	sd_bus_error *berr)
+{
+	bool previously;
+	peer *peer;
+	ctx *ctx;
+	int rc;
+
+	peer = data;
+	ctx = peer->ctx;
+	previously = peer->degraded;
+
+	if (!previously) {
+		assert(!peer->recovery.delay);
+		assert(!peer->recovery.source);
+		assert(!peer->recovery.npolls);
+		peer->recovery.npolls = MCTP_I2C_TSYM_MN1_MIN + 1;
+		peer->recovery.delay =
+			(MCTP_I2C_TSYM_TRECLAIM_MIN_US / 2) - ctx->mctp_timeout;
+		rc = sd_event_add_time_relative(ctx->event, &peer->recovery.source,
+			CLOCK_MONOTONIC, 0, ctx->mctp_timeout, peer_endpoint_recover, peer);
+		if (rc < 0) {
+			goto out;
+		}
+
+		peer->degraded = true;
+
+		rc = sd_bus_emit_properties_changed(sd_bus_message_get_bus(call),
+				sd_bus_message_get_path(call),
+				sd_bus_message_get_interface(call),
+				"Connectivity",
+				NULL);
+		if (rc < 0) {
+			goto out;
+		}
+	}
+
+	rc = sd_bus_reply_method_return(call, NULL);
+
+out:
+	if (rc < 0 && !previously) {
+		if (peer->degraded) {
+			/* Cleanup the timer if it was setup successfully. */
+			sd_event_source_set_enabled(peer->recovery.source, SD_EVENT_OFF);
+			sd_event_source_unref(peer->recovery.source);
+		}
+		peer->degraded = previously;
+		peer->recovery.delay = 0;
+		peer->recovery.source = NULL;
+		peer->recovery.npolls = 0;
+	}
+	set_berr(ctx, rc, berr);
+	return rc;
+}
+
 static int method_endpoint_set_mtu(sd_bus_message *call, void *data,
 	sd_bus_error *berr)
 {
@@ -2337,6 +2601,8 @@ static int bus_endpoint_get_prop(sd_bus *bus,
 	} else if (strcmp(property, "UUID") == 0 && peer->uuid) {
 		const char *s = dfree(bytes_to_uuid(peer->uuid));
 		rc = sd_bus_message_append(reply, "s", s);
+	} else if (strcmp(property, "Connectivity") == 0) {
+		rc = sd_bus_message_append(reply, "s", peer->degraded ? "Degraded" : "Available");
 	} else {
 		printf("Unknown property '%s' for %s iface %s\n", property, path, interface);
 		rc = -ENOENT;
@@ -2387,6 +2653,16 @@ static const sd_bus_vtable bus_endpoint_cc_vtable[] = {
 		SD_BUS_NO_RESULT,
 		method_endpoint_remove,
 		0),
+	SD_BUS_METHOD("Recover",
+		SD_BUS_NO_ARGS,
+		SD_BUS_NO_RESULT,
+		method_endpoint_recover,
+		0),
+	SD_BUS_PROPERTY("Connectivity",
+		"s",
+		bus_endpoint_get_prop,
+		0,
+		SD_BUS_VTABLE_PROPERTY_EMITS_CHANGE),
 	SD_BUS_VTABLE_END
 };
 

commit 498f73e85beaeb235a49aa86407af0ef60cca6ec

mctpd: add_peer: Update userdata pointer for active event sources
Mitigate the issue reported via [1] and highlighted by ASAN:

    ==179005==ERROR: AddressSanitizer: heap-use-after-free on address 0x61f000000c38 at pc 0x55dfaa7fa308 bp 0x7ffe10264420 sp 0x7ffe10264418
    READ of size 8 at 0x61f000000c38 thread T0
        0 0x55dfaa7fa307 in peer_endpoint_recover ../src/mctpd.c:2570
        1 0x7f9a43dadae3  (/lib/x86_64-linux-gnu/libsystemd.so.0+0x78ae3)
        2 0x7f9a43dade04 in sd_event_dispatch (/lib/x86_64-linux-gnu/libsystemd.so.0+0x78e04)
        3 0x7f9a43daf2e7 in sd_event_run (/lib/x86_64-linux-gnu/libsystemd.so.0+0x7a2e7)
        4 0x7f9a43daf506 in sd_event_loop (/lib/x86_64-linux-gnu/libsystemd.so.0+0x7a506)
        5 0x55dfaa80a609 in main ../src/mctpd.c:4547
        6 0x7f9a42c46249 in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58
        7 0x7f9a42c46304 in __libc_start_main_impl ../csu/libc-start.c:360
        8 0x55dfaa7e38d0 in _start (mctp/build/test-mctpd+0x688d0)

    0x61f000000c38 is located 3000 bytes inside of 3040-byte region [0x61f000000080,0x61f000000c60)
    freed by thread T0 here:
        0 0x7f9a436b78d5 in __interceptor_realloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:85
        1 0x55dfaa7ef028 in add_peer ../src/mctpd.c:1419
        2 0x55dfaa7f1587 in endpoint_assign_eid ../src/mctpd.c:1601
        3 0x55dfaa7f55a0 in method_setup_endpoint ../src/mctpd.c:2038
        4 0x7f9a43d650ad  (/lib/x86_64-linux-gnu/libsystemd.so.0+0x300ad)

    previously allocated by thread T0 here:
        0 0x7f9a436b78d5 in __interceptor_realloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:85
        1 0x55dfaa7ef028 in add_peer ../src/mctpd.c:1419
        2 0x55dfaa805741 in add_local_eid ../src/mctpd.c:4052
        3 0x55dfaa80627f in add_interface_local ../src/mctpd.c:4114
        4 0x55dfaa806ffa in setup_nets ../src/mctpd.c:4200
        5 0x55dfaa80a380 in main ../src/mctpd.c:4525
        6 0x7f9a42c46249 in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58

    SUMMARY: AddressSanitizer: heap-use-after-free ../src/mctpd.c:2570 in peer_endpoint_recover
    Shadow bytes around the buggy address:
      0x0c3e7fff8130: fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd
      0x0c3e7fff8140: fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd
      0x0c3e7fff8150: fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd
      0x0c3e7fff8160: fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd
      0x0c3e7fff8170: fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd fd
    =>0x0c3e7fff8180: fd fd fd fd fd fd fd[fd]fd fd fd fd fa fa fa fa
      0x0c3e7fff8190: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c3e7fff81a0: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c3e7fff81b0: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c3e7fff81c0: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c3e7fff81d0: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
    Shadow byte legend (one shadow byte represents 8 application bytes):
      Addressable:           00
      Partially addressable: 01 02 03 04 05 06 07
      Heap left redzone:       fa
      Freed heap region:       fd
      Stack left redzone:      f1
      Stack mid redzone:       f2
      Stack right redzone:     f3
      Stack after return:      f5
      Stack use after scope:   f8
      Global redzone:          f9
      Global init order:       f6
      Poisoned by user:        f7
      Container overflow:      fc
      Array cookie:            ac
      Intra object redzone:    bb
      ASan internal:           fe
      Left alloca redzone:     ca
      Right alloca redzone:    cb
    ==179005==ABORTING

Link: https://github.com/codeconstruct/mctp/issues/69 [1]
Fixes: 7ec2f8daa3a8 ("mctpd: Add support for endpoint recovery")

src/mctpd.c
@@ -1423,6 +1423,12 @@ static int add_peer(ctx *ctx, const dest_phys *dest, mctp_eid_t eid,
 		memset(&ctx->peers[ctx->size_peers], 0x0,
 			sizeof(*ctx->peers) * (new_size - ctx->size_peers));
 		ctx->size_peers = new_size;
+		// Update user data for recovery tasks
+		for (size_t ridx = 0; ridx < ctx->size_peers; ridx++) {
+			peer = &ctx->peers[ridx];
+			if (sd_event_source_get_enabled(peer->recovery.source, NULL) > 0)
+				sd_event_source_set_userdata(peer->recovery.source, peer);
+		}
 	}
 
 	// Populate it

tests/test_mctpd.py
@@ -483,3 +483,68 @@ async def test_network_local_eids_none(dbus, mctpd):
     eids = list(await net.get_local_eids())
 
     assert eids == []
+
+async def test_concurrent_recovery_setup(dbus, mctpd):
+    iface = mctpd.system.interfaces[0]
+    mctp_i = await mctpd_mctp_iface_obj(dbus, iface)
+
+    # mctpd context tracks 20 peer objects by default, add and set up 19 so we
+    # reach the allocation boundary.
+    split = 19
+    for i in range(split):
+        pep = Endpoint(iface, bytes([0x1e + i]))
+        mctpd.network.add_endpoint(pep)
+        (_, _, path, _) = await mctp_i.call_setup_endpoint(pep.lladdr)
+
+    # Grab the DBus path for an endpoint that we will cause to be removed from
+    # the network through the recovery path. Arbitrarily use the most recent
+    # one added
+    ep = await dbus.get_proxy_object(MCTPD_C, path)
+    ep_props = await ep.get_interface(DBUS_PROPERTIES_I)
+
+    # Set up a match for Connectivity transitioning to Degraded on the endpoint
+    # for which we request recovery
+    degraded = trio.Semaphore(initial_value = 0)
+    def ep_connectivity_changed(iface, changed, invalidated):
+        if iface == MCTPD_ENDPOINT_I and 'Connectivity' in changed:
+            if 'Degraded' == changed['Connectivity'].value:
+                degraded.release()
+    await ep_props.on_properties_changed(ep_connectivity_changed)
+
+    # Set up a match for the recovery endpoint object being removed from DBus
+    mctp_p = await dbus.get_proxy_object(MCTPD_C, MCTPD_MCTP_P)
+    mctp_objmgr = await mctp_p.get_interface(DBUS_OBJECT_MANAGER_I)
+    removed = trio.Semaphore(initial_value = 0)
+    def ep_removed(ep_path, interfaces):
+        if ep_path == path and MCTPD_ENDPOINT_I in interfaces:
+            removed.release()
+
+    await mctp_objmgr.on_interfaces_removed(ep_removed)
+
+    # Delete the endpoint from the network so its recovery will fail after
+    # timeout. Note we delete at the split index as the network was already
+    # populated with the default endpoint
+    del mctpd.network.endpoints[split]
+
+    # Begin recovery for the endpoint ...
+    ep_ep = await ep.get_interface(MCTPD_ENDPOINT_I)
+    await ep_ep.call_recover()
+
+    # ... and wait until we're notified the recovery process has begun
+    with trio.move_on_after(1) as expected:
+        await degraded.acquire()
+    assert not expected.cancelled_caught
+
+    # Now that we're asynchronously waiting for the endpoint recovery process
+    # to complete, force a realloc() of the peer object array by adding a new
+    # peer, which will invalidate the recovering peer's pointer
+    pep = Endpoint(iface, bytes([0x1e + split]))
+    mctpd.network.add_endpoint(pep)
+    (_, _, _, new) = await mctp_i.call_setup_endpoint(pep.lladdr)
+    assert new
+
+    # Verify the recovery process completed gracefully with removal of the
+    # endpoint's DBus object
+    with trio.move_on_after(2 * MCTPD_TRECLAIM) as expected:
+        await removed.acquire()
+    assert not expected.cancelled_caught