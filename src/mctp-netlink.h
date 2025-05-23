74a3740fbe54f4139d58de77d21f0e69be4bd08a
Jeremy Kerr <jk@codeconstruct.com.au>
2025-05-23 13:31:33 +0800

    mctp, mctp-netlink: allow add & remove of range routes
    Add a separate parser for the route target argument, allowing
    <min>-<max> ranges to be specified, and pass to the rtm_dst_len value of
    the netlink rtmsg.
    
    Fixes: https://github.com/CodeConstruct/mctp/issues/58
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- src/mctp-netlink.h
+++ src/mctp-netlink.h
@@ -99,10 +99,11 @@ void *mctp_nl_get_link_userdata(const mctp_nl *nl, int ifindex);
 void *mctp_nl_get_link_userdata_byname(const mctp_nl *nl, const char *ifname);
 
 /* MCTP route helper */
-int mctp_nl_route_add(struct mctp_nl *nl, uint8_t eid, const char* ifname,
-		      const struct mctp_fq_addr *gw, uint32_t mtu);
-int mctp_nl_route_del(struct mctp_nl *nl, uint8_t eid, const char* ifname,
-		      const struct mctp_fq_addr *gw);
+int mctp_nl_route_add(struct mctp_nl *nl, uint8_t eid, unsigned int extent,
+		      const char* ifname, const struct mctp_fq_addr *gw,
+		      uint32_t mtu);
+int mctp_nl_route_del(struct mctp_nl *nl, uint8_t eid, unsigned int extent,
+		      const char* ifname, const struct mctp_fq_addr *gw);
 
 /* Helpers */
 
