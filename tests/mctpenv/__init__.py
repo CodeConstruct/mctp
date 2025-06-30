93e7c6e53ed5ecbc60714a67f08ba9bb4483341c
Faizan Ali <faizana@nvidia.com>
2025-07-01 01:45:11 +0530

    Add tests for dynamic bridge eid and downstream eid assignment
    Add new test for validating AssignEndpoint D-Bus method
    to verify bridge endpoint EID allocation being contiguous to its
    downstream eids. Add Allocate Endpoint control message support with
    new endpoint property for allocated pool size also assign dynamic eid
    contiguous to bridge during Allocate Endpoint control message.
    
    Signed-off-by: Faizan Ali <faizana@nvidia.com>

--- tests/mctpenv/__init__.py
+++ tests/mctpenv/__init__.py
@@ -303,6 +303,9 @@ class Endpoint:
         self.eid = eid
         self.types = types or [0]
         self.bridged_eps = []
+        self.pool_size = 0
+        self.allocated_pool = None # [start, size]
+
         # keyed by (type, type-specific-instance)
         self.commands = {}
 
@@ -348,7 +351,9 @@ class Endpoint:
                 # Set Endpoint ID
                 (op, eid) = data[2:]
                 self.eid = eid
-                data = bytes(hdr + [0x00, 0x00, self.eid, 0x00])
+                self.pool_size = len(self.bridged_eps)
+                alloc_status = 0x01 if self.pool_size > 0 else 0x00
+                data = bytes(hdr + [0x00, alloc_status, self.eid, self.pool_size])
                 await sock.send(raddr, data)
 
             elif opcode == 2:
@@ -365,6 +370,23 @@ class Endpoint:
                 # Get Message Type Support
                 types = self.types
                 data = bytes(hdr + [0x00, len(types)] + types)
+                await sock.send(raddr, data)
+
+            elif opcode == 8:
+                # Allocate Endpoint IDs
+                (_, _, _, pool_size, pool_start) = data
+                alloc_status = 0x00
+                if self.allocated_pool is not None:
+                    alloc_status = 0x01
+                else:
+                    num_eps = min(pool_size, self.pool_size)
+                    self.allocated_pool = [pool_start, num_eps]
+                    # Assign sequential EIDs starting from pool_start
+                    for ep in range(num_eps):
+                        self.bridged_eps[ep].eid = self.allocated_pool[0] + ep
+
+                data = bytes(hdr + [0x00, alloc_status,
+                                self.allocated_pool[1], self.allocated_pool[0]])
                 await sock.send(raddr, data)
 
             else:
