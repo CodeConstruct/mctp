bb0dff12b59fa6ef2b11fb2f348949829badd8f1
Khang D Nguyen <khangng@os.amperecomputing.com>
2025-08-28 13:09:14 +0700

    mctpd: Send Discovery Notify on Endpoint role set
    This commit adds support for Discovery Notify messages, specified in
    DSP0236 section 12.14.
    
    In our implementation, a Discovery Notify message is sent when mctpd
    sets an interface role from Unknown to Endpoint.
    
    To avoid notify discovery messages getting lost, retry the messages for
    a few time with delays.
    
    Signed-off-by: Khang D Nguyen <khangng@os.amperecomputing.com>

--- tests/test_mctpd_endpoint.py
+++ tests/test_mctpd_endpoint.py
@@ -22,11 +22,16 @@ async def iface():
 
 
 @pytest.fixture
-async def sysnet(iface):
+async def bo(iface):
+    return Endpoint(iface, bytes([0x10]), eid=8)
+
+
+@pytest.fixture
+async def sysnet(iface, bo):
     system = System()
     await system.add_interface(iface)
     network = Network()
-    network.add_endpoint(Endpoint(iface, bytes([0x10]), eid=8))
+    network.add_endpoint(bo)
     return Sysnet(system, network)
 
 
@@ -113,11 +118,99 @@ class TestDiscovery:
     async def iface(self):
         return System.Interface("mctp0", 1, 1, bytes([0x1D]), 68, 254, True, PhysicalBinding.PCIE_VDM)
 
+    @pytest.fixture
+    async def bo(self, iface):
+        return TestDiscovery.BusOwnerEndpoint(iface, bytes([0x00]), eid=8)
+
+
+    class BusOwnerEndpoint(Endpoint):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.sem = trio.Semaphore(initial_value=0)
+
+        async def handle_mctp_control(self, sock, addr, data):
+            print(addr, data)
+            flags, opcode = data[0:2]
+            if opcode != 0x0D:
+                return await super().handle_mctp_control(sock, addr, data)
+            dst_addr = MCTPSockAddr.for_ep_resp(self, addr, sock.addr_ext)
+            await sock.send(dst_addr, bytes([flags & 0x1F, opcode, 0x00]))
+            self.sem.release()
+
+
     """ Test simple Discovery sequence """
     async def test_simple_discovery_sequence(self, dbus, mctpd):
         bo = mctpd.network.endpoints[0]
 
         assert len(mctpd.system.addresses) == 0
+
+        # BMC should send a Discovery Notify message
+        with trio.move_on_after(5) as expected:
+            await bo.sem.acquire()
+        assert not expected.cancelled_caught
+
+        # no EID yet
+        rsp = await bo.send_control(mctpd.network.mctp_socket, MCTPControlCommand(True, 0, 0x02))
+        assert rsp.hex(' ') == '00 02 00 00 02 00'
+
+        # BMC response to Prepare for Discovery
+        rsp = await bo.send_control(mctpd.network.mctp_socket, MCTPControlCommand(True, 0, 0x0B))
+        assert rsp.hex(' ') == '00 0b 00'
+
+        # BMC response to Endpoint Discovery
+        rsp = await bo.send_control(mctpd.network.mctp_socket, MCTPControlCommand(True, 0, 0x0C))
+        assert rsp.hex(' ') == '00 0c 00'
+
+        # set EID = 42
+        eid = 42
+        rsp = await bo.send_control(mctpd.network.mctp_socket, MCTPControlCommand(True, 0, 0x01, bytes([0x00, eid])))
+        assert rsp.hex(' ') == f'00 01 00 00 {eid:02x} 00'
+
+        # BMC should contains two object paths: bus owner and itself
+        assert await mctpd_mctp_endpoint_control_obj(dbus, f"/au/com/codeconstruct/mctp1/networks/1/endpoints/{bo.eid}")
+        assert await mctpd_mctp_endpoint_control_obj(dbus, f"/au/com/codeconstruct/mctp1/networks/1/endpoints/{eid}")
+
+
+class TestDiscoveryRetry:
+    @pytest.fixture
+    async def iface(self):
+        return System.Interface("mctp0", 1, 1, bytes([0x1D]), 68, 254, True, PhysicalBinding.PCIE_VDM)
+
+    @pytest.fixture
+    async def bo(self, iface):
+        return TestDiscoveryRetry.BusOwnerEndpoint(iface, bytes([0x00]), eid=8)
+
+
+    class BusOwnerEndpoint(Endpoint):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.sem = trio.Semaphore(initial_value=0)
+            self.retry_left = 1
+
+        async def handle_mctp_control(self, sock, src_addr, msg):
+            flags, opcode = msg[0:2]
+            if opcode != 0x0D:
+                return await super().handle_mctp_control(sock, src_addr, msg)
+
+            # only reply after 2 retries
+            if self.retry_left == 0:
+                dst_addr = MCTPSockAddr.for_ep_resp(self, src_addr, sock.addr_ext)
+                await sock.send(dst_addr, bytes([flags & 0x1F, opcode, 0x00]))
+                self.sem.release()
+            else:
+                self.retry_left -= 1
+
+
+    """ Test simple Discovery sequence """
+    async def test_discovery_after_one_retry(self, dbus, mctpd, autojump_clock):
+        bo = mctpd.network.endpoints[0]
+
+        assert len(mctpd.system.addresses) == 0
+
+        # BMC should send a Discovery Notify message
+        with trio.move_on_after(10) as expected:
+            await bo.sem.acquire()
+        assert not expected.cancelled_caught
 
         # no EID yet
         rsp = await bo.send_control(mctpd.network.mctp_socket, MCTPControlCommand(True, 0, 0x02))
