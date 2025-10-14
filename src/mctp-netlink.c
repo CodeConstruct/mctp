CodeConstruct/mctp

commit 1b6c3673b40cd8bd906d5619272d85cc433a8214

mctp-netlink: add MCTP_NL_CHANGE_NAME event
We would like to handle interface renames, so handle cases where we see
netlink link data with a new name, and populate the change accordingly.

Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctp-netlink.c
@@ -301,6 +301,16 @@ static void fill_link_changes(const struct linkmap_entry *old, size_t old_count,
 				ch->old_up = oe->up;
 				ch->link_userdata = oe->userdata;
 			}
+
+			if (strncmp(oe->ifname, ne->ifname,
+				    sizeof(oe->ifname))) {
+				ch = push_change(changes, &siz);
+				ch->op = MCTP_NL_CHANGE_NAME;
+				ch->ifindex = ne->ifindex;
+				memcpy(ch->old_name, ne->ifname,
+				       sizeof(ch->old_name));
+				ch->link_userdata = oe->userdata;
+			}
 			o++;
 			n++;
 		} else if (!oe || (ne && ne->ifindex < oe->ifindex)) {
@@ -343,9 +353,9 @@ void mctp_nl_changes_dump(mctp_nl *nl, mctp_nl_change *changes,
 		if (!ifname)
 			ifname = "deleted";
 		fprintf(stderr,
-			"%3zd %-12s ifindex %3d (%-20s) eid %3d old_net %4d old_up %d\n",
+			"%3zd %-12s ifindex %3d (%-20s) eid %3d old_net %4d old_up %d old name %s\n",
 			i, ops[ch->op], ch->ifindex, ifname, ch->eid,
-			ch->old_net, ch->old_up);
+			ch->old_net, ch->old_up, ch->old_name);
 	}
 }
 

src/mctp-netlink.h
@@ -17,6 +17,7 @@ struct mctp_nl_change {
 		MCTP_NL_DEL_LINK,
 		MCTP_NL_CHANGE_NET,
 		MCTP_NL_CHANGE_UP,
+		MCTP_NL_CHANGE_NAME,
 		MCTP_NL_ADD_EID,
 		MCTP_NL_DEL_EID,
 	} op;
@@ -32,8 +33,11 @@ struct mctp_nl_change {
 	// Filled for CHANGE_UP
 	bool old_up;
 
+	// Filled for CHANGE_NAME
+	char old_name[IFNAMSIZ + 1];
+
 	// If userdata is present on the link, it is passed here. Populated for
-	// link change events (DEL_LINK, CHANGE_NET, CHANGE_UP).
+	// link change events (DEL_LINK, CHANGE_NET, CHANGE_UP, CHANGE_NAME).
 	void *link_userdata;
 };
 typedef struct mctp_nl_change mctp_nl_change;

commit 3506e7d9ab35c422f3729b4be356bce002260427

netlink: Fix NL_OP_COUNT definition, add debug for CHANGE_NAME
Commit 1b6c367 ("mctp-netlink: add MCTP_NL_CHANGE_NAME event") did not
update the COUNT definition, so causing a potential array index overrun
on change dump.

Instead of hard-coding, use the enum itself for the count value, and add
a dump string for the new CHANGE_NAME event type.

Fixes: 1b6c367 ("mctp-netlink: add MCTP_NL_CHANGE_NAME event")
Fixes: https://github.com/CodeConstruct/mctp/issues/120
Reported-by: Nidhin MS <nidhin.ms@intel.com>
Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctp-netlink.c
@@ -345,8 +345,8 @@ void mctp_nl_changes_dump(mctp_nl *nl, mctp_nl_change *changes,
 			  size_t num_changes)
 {
 	const char *ops[MCTP_NL_OP_COUNT] = {
-		"ADD_LINK",  "DEL_LINK", "CHANGE_NET",
-		"CHANGE_UP", "ADD_EID",	 "DEL_EID",
+		"ADD_LINK",    "DEL_LINK", "CHANGE_NET", "CHANGE_UP",
+		"CHANGE_NAME", "ADD_EID",  "DEL_EID",
 	};
 
 	fprintf(stderr, "%zu changes:\n", num_changes);

src/mctp-netlink.h
@@ -13,7 +13,7 @@ struct mctp_nl;
 typedef struct mctp_nl mctp_nl;
 
 struct mctp_nl_change {
-#define MCTP_NL_OP_COUNT 6
+#define MCTP_NL_OP_COUNT __MCTP_NL_OP_MAX
 	enum {
 		MCTP_NL_ADD_LINK,
 		MCTP_NL_DEL_LINK,
@@ -22,6 +22,7 @@ struct mctp_nl_change {
 		MCTP_NL_CHANGE_NAME,
 		MCTP_NL_ADD_EID,
 		MCTP_NL_DEL_EID,
+		__MCTP_NL_OP_MAX,
 	} op;
 
 	int ifindex;

src/mctpd.c
@@ -1361,6 +1361,8 @@ static int cb_listen_monitor(sd_event_source *s, int sd, uint32_t revents,
 			// 'up' state is currently unused
 			break;
 		}
+		default:
+			bug_warn("Unhandled netlink change type %d", c->op);
 		}
 	}
 