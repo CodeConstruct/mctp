CodeConstruct/mctp

commit 83cbe46e7c1b9f3cde2e29c4332710a014e117d5

mctpd: remove add_peer_route() from setup_added_peer()
setup_added_peer() does a few things:

 - adds the local route
 - queries the peer for UUID and message types
 - publishes the peer to dbus

We will need the local route established earlier in some cases, so move
this out of setup_added_peer.

We have six current callers:

 - peer_endpoint_poll(), which already relies on a bridge pool route to
   be present

 - the endpoint Set Endpoint ID handler, where we add a new
   add_peer_route()

 - endpoint_assign_eid() and endpoint_assign_bridge_static_eid,
   which both already call add_peer_route()

 - get_endpoint_peer(), which only needs a route added in some cases,
   where the endpoint was not previously known (the change_peer_eid()
   will also set up the new route)

 - method_setup_endpoint, where we add a new add_peer_route()

This involves correcting the route counts in a couple of test cases:
these are was expecting two routes to be present (assumed to be the
local route and the new peer route), but they are duplicates, as a
result of the existing add_peer_route(), setup_added_peer() in the
existing Set Endpoint ID handler.

Conveniently, the setup_added_peer()'s comment will now match the
implementation.

Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -899,6 +899,7 @@ static int handle_control_set_endpoint_id(struct ctx *ctx, int sd,
 
 		rc = add_peer_from_addr(ctx, addr, &peer);
 		if (rc == 0) {
+			add_peer_route(peer);
 			rc = setup_added_peer(peer);
 		}
 		if (rc < 0) {
@@ -2713,6 +2714,8 @@ static int get_endpoint_peer(struct ctx *ctx, sd_bus_error *berr,
 		rc = add_peer(ctx, dest, eid, net, &peer, false);
 		if (rc < 0)
 			return rc;
+
+		add_peer_route(peer);
 	}
 
 	peer->endpoint_type = ep_type;
@@ -3092,6 +3095,7 @@ static int method_setup_endpoint(sd_bus_message *call, void *data,
 		} else if (rc < 0) {
 			goto err;
 		} else {
+			add_peer_route(peer);
 			peer->endpoint_type = ep_type;
 			peer->medium_spec = medium_spec;
 			rc = setup_added_peer(peer);
@@ -3509,11 +3513,6 @@ static int setup_added_peer(struct peer *peer)
 	// Set minimum MTU by default for compatibility. Clients can increase
 	// this with .SetMTU as needed
 	peer->mtu = mctp_nl_min_mtu_byindex(peer->ctx->nl, peer->phys.ifindex);
-
-	// add route before querying for non-bridged endpoints.
-	// bridged endpoints will use the bridge's pool range route.
-	if (!is_eid_in_bridge_pool(n, peer->ctx, peer->eid))
-		add_peer_route(peer);
 
 	rc = query_peer_properties(peer);
 	if (rc < 0)

tests/test_mctpd.py
@@ -127,7 +127,7 @@ async def test_setup_endpoint(dbus, mctpd):
     assert neigh.eid == ep.eid
 
     # we should have a route for the new endpoint too
-    assert len(mctpd.system.routes) == 2
+    assert len(mctpd.system.routes) == 1
 
 
 async def test_setup_endpoint_conflict(dbus, mctpd):
@@ -356,7 +356,7 @@ async def test_assign_endpoint_static(dbus, mctpd):
     neigh = mctpd.system.neighbours[0]
     assert neigh.lladdr == dev.lladdr
     assert neigh.eid == static_eid
-    assert len(mctpd.system.routes) == 2
+    assert len(mctpd.system.routes) == 1
 
 
 async def test_assign_endpoint_static_allocated(dbus, mctpd):

commit f033b5f6f9fd88413a2f8cadfbe59283ae94ab34

mctpd: Set default peer MTU earlier
Commit 83cbe46e moved peer route creation to an earlier point, before
the peer->mtu had been set in setup_added_peer(). This means we never
use the minimum MTU value in created routes, so lose the safety of a
known-good MTU setting.

The setup_added_peer() path is really the wrong place for MTU
initialisation; this is for property querying. Instead, set the initial
MTU when the peer is constructed.

Fixes: 83cbe46e ("mctpd: remove add_peer_route() from setup_added_peer()")
Reported-by: Brian McKenzie <Brian.McKenzie@amd.com>
Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -2048,6 +2048,10 @@ static int add_peer(struct ctx *ctx, const dest_phys *dest, mctp_eid_t eid,
 	peer->state = REMOTE;
 	peer->ctx = ctx;
 
+	// Set minimum MTU by default for compatibility. Clients can increase
+	// this with .SetMTU as needed
+	peer->mtu = mctp_nl_min_mtu_byindex(ctx->nl, peer->phys.ifindex);
+
 	// Update network eid map
 	n->peers[eid] = peer;
 
@@ -3531,9 +3535,6 @@ static int setup_added_peer(struct peer *peer)
 		bug_warn("%s Bad net %u", __func__, peer->net);
 		return -EPROTO;
 	}
-	// Set minimum MTU by default for compatibility. Clients can increase
-	// this with .SetMTU as needed
-	peer->mtu = mctp_nl_min_mtu_byindex(peer->ctx->nl, peer->phys.ifindex);
 
 	rc = query_peer_properties(peer);
 	if (rc < 0)

tests/test_mctpd.py
@@ -216,6 +216,22 @@ async def test_setup_endpoint_no_get_uuid(dbus, mctpd):
     assert eid == ep.eid
 
 
+async def test_setup_endpoint_mtu(dbus, mctpd):
+    """Newly-added endpoints should start with a minimum MTU"""
+    iface = mctpd.system.interfaces[0]
+    ep = mctpd.network.endpoints[0]
+
+    # ensure we can distinguish a correct min mtu from a no-mtu case
+    assert iface.min_mtu != 0
+
+    mctp = await mctpd_mctp_iface_obj(dbus, iface)
+    (eid, net, path, new) = await mctp.call_setup_endpoint(ep.lladdr)
+
+    assert len(mctpd.system.routes) == 1
+    route = mctpd.system.routes[0]
+    assert route.mtu == iface.min_mtu
+
+
 async def test_remove_endpoint(dbus, mctpd):
     """Test neighbour removal"""
     iface = mctpd.system.interfaces[0]