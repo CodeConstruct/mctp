CodeConstruct/mctp

commit 3f3efb8d15200168bbec0702faa634ccd1d316eb

mctpd: add endpoint polling interval configuration
Refering to DSP0236 sec 8.17.6 Reclaiming EIDs from hot-plug devices

Requirement: The bus owner/bridge needs to periodically poll
GET_ENDPOINT_ID control command to check if downstream endpoint
is accessible. Once it's established that endpoin is accessible,
polling needs to stop.

Introduce new endpoint polling configuration to be used as periodic
interval for sending poll command GET_ENDPOINT_ID by bus owner/bridge.
Disable bridge poll via setting poll time as zero.

Signed-off-by: Faizan Ali <faizana@nvidia.com>

src/mctpd.c
@@ -59,6 +59,8 @@ static const char *mctpd_appid = "67369c05-4b97-4b7e-be72-65cfd8639f10";
 
 static const char *conf_file_default = MCTPD_CONF_FILE_DEFAULT;
 
+static const uint64_t max_poll_interval_ms = 10000;
+static const uint64_t min_poll_interval_ms = 2500;
 static const mctp_eid_t eid_alloc_min = 0x08;
 static const mctp_eid_t eid_alloc_max = 0xfe;
 static const uint8_t MCTP_TYPE_VENDOR_PCIE = 0x7e;
@@ -268,6 +270,10 @@ struct ctx {
 
 	//  maximum pool size for assumed MCTP Bridge
 	uint8_t max_pool_size;
+
+	// bus owner/bridge polling interval in usecs for
+	// checking endpoint's accessibility.
+	uint64_t endpoint_poll;
 };
 
 static int emit_endpoint_added(const struct peer *peer);
@@ -5050,6 +5056,18 @@ static int parse_config_bus_owner(struct ctx *ctx, toml_table_t *bus_owner)
 			return rc;
 	}
 
+	val = toml_int_in(bus_owner, "endpoint_poll_ms");
+	if (val.ok && val.u.i) {
+		uint64_t i = val.u.i;
+		if ((i > max_poll_interval_ms) || (i < min_poll_interval_ms)) {
+			warnx("endpoint polling interval invalid (%lu - %lu ms)",
+			      min_poll_interval_ms, max_poll_interval_ms);
+			return -1;
+		}
+
+		ctx->endpoint_poll = i * 1000;
+	}
+
 	return 0;
 }
 
@@ -5154,6 +5172,7 @@ static void setup_config_defaults(struct ctx *ctx)
 	ctx->max_pool_size = 15;
 	ctx->dyn_eid_min = eid_alloc_min;
 	ctx->dyn_eid_max = eid_alloc_max;
+	ctx->endpoint_poll = 0;
 }
 
 static void free_config(struct ctx *ctx)

conf/mctpd.conf
@@ -16,3 +16,7 @@ message_timeout_ms = 30
 dynamic_eid_range = [8, 254]
 
 max_pool_size = 15
+
+# Bus Owner/bridge polling interval (ms) to check endpoint accessibility.
+# A value of 0 disables polling.
+endpoint_poll_ms = 0

commit ee818530c2a8718f92e83bbd9a0ab3a2bb75ff63

mctpd: fix format-string warnings on 32-bit
Building on 64-bit-is-long-long platforms produces a couple of warnings
when formatting a uint64_t:

  src/mctpd.c:5697:69: warning: format ‘%lu’ expects argument of type
  ‘long unsigned int’, but argument 2 has type ‘uint64_t’ {aka ‘long
  long unsigned int’} [-Wformat=]

Instead of %lu, (begrudginly) use the PRIu64 macro for the format string

Fixes: 3f3efb8 ("mctpd: add endpoint polling interval configuration")
Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

src/mctpd.c
@@ -28,6 +28,7 @@
 #include <err.h>
 #include <errno.h>
 #include <fnmatch.h>
+#include <inttypes.h>
 #include <getopt.h>
 #include <signal.h>
 
@@ -5694,7 +5695,8 @@ static int parse_config_bus_owner(struct ctx *ctx, toml_table_t *bus_owner)
 	if (val.ok && val.u.i) {
 		uint64_t i = val.u.i;
 		if ((i > max_poll_interval_ms) || (i < min_poll_interval_ms)) {
-			warnx("endpoint polling interval invalid (%lu - %lu ms)",
+			warnx("endpoint polling interval invalid "
+			      "(%" PRIu64 " - %" PRIu64 " ms)",
 			      min_poll_interval_ms, max_poll_interval_ms);
 			return -1;
 		}